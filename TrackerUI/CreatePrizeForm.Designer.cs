0fcd270b3e317131aa1a5e9a13846bfb8f87a0c1
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-16 12:21:38 +0700

    Design Create Prize Form

---
+++ TrackerUI/CreatePrizeForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TrackerUI
+{
+    partial class CreatePrizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbHeader = new System.Windows.Forms.Label();
+            this.txtPlaceNumber = new System.Windows.Forms.TextBox();
+            this.lbPlaceNumber = new System.Windows.Forms.Label();
+            this.txtPlaceName = new System.Windows.Forms.TextBox();
+            this.lbPlaceName = new System.Windows.Forms.Label();
+            this.txtPlaceAmount = new System.Windows.Forms.TextBox();
+            this.lbPrizeAmount = new System.Windows.Forms.Label();
+            this.txtPrizePercentage = new System.Windows.Forms.TextBox();
+            this.lbPrizePercentage = new System.Windows.Forms.Label();
+            this.lbOr = new System.Windows.Forms.Label();
+            this.btnCreatePrize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbHeader
+            // 
+            this.lbHeader.AutoSize = true;
+            this.lbHeader.Font = new System.Drawing.Font("Segoe UI Light", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbHeader.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbHeader.Location = new System.Drawing.Point(12, 9);
+            this.lbHeader.Name = "lbHeader";
+            this.lbHeader.Size = new System.Drawing.Size(209, 50);
+            this.lbHeader.TabIndex = 12;
+            this.lbHeader.Text = "Create Prize";
+            // 
+            // txtPlaceNumber
+            // 
+            this.txtPlaceNumber.Location = new System.Drawing.Point(236, 80);
+            this.txtPlaceNumber.Name = "txtPlaceNumber";
+            this.txtPlaceNumber.Size = new System.Drawing.Size(178, 35);
+            this.txtPlaceNumber.TabIndex = 22;
+            // 
+            // lbPlaceNumber
+            // 
+            this.lbPlaceNumber.AutoSize = true;
+            this.lbPlaceNumber.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPlaceNumber.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbPlaceNumber.Location = new System.Drawing.Point(18, 78);
+            this.lbPlaceNumber.Name = "lbPlaceNumber";
+            this.lbPlaceNumber.Size = new System.Drawing.Size(183, 37);
+            this.lbPlaceNumber.TabIndex = 21;
+            this.lbPlaceNumber.Text = "Place Number";
+            // 
+            // txtPlaceName
+            // 
+            this.txtPlaceName.Location = new System.Drawing.Point(236, 132);
+            this.txtPlaceName.Name = "txtPlaceName";
+            this.txtPlaceName.Size = new System.Drawing.Size(178, 35);
+            this.txtPlaceName.TabIndex = 24;
+            // 
+            // lbPlaceName
+            // 
+            this.lbPlaceName.AutoSize = true;
+            this.lbPlaceName.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPlaceName.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbPlaceName.Location = new System.Drawing.Point(18, 129);
+            this.lbPlaceName.Name = "lbPlaceName";
+            this.lbPlaceName.Size = new System.Drawing.Size(157, 37);
+            this.lbPlaceName.TabIndex = 23;
+            this.lbPlaceName.Text = "Place Name";
+            // 
+            // txtPlaceAmount
+            // 
+            this.txtPlaceAmount.Location = new System.Drawing.Point(236, 182);
+            this.txtPlaceAmount.Name = "txtPlaceAmount";
+            this.txtPlaceAmount.Size = new System.Drawing.Size(178, 35);
+            this.txtPlaceAmount.TabIndex = 26;
+            // 
+            // lbPrizeAmount
+            // 
+            this.lbPrizeAmount.AutoSize = true;
+            this.lbPrizeAmount.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPrizeAmount.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbPrizeAmount.Location = new System.Drawing.Point(18, 179);
+            this.lbPrizeAmount.Name = "lbPrizeAmount";
+            this.lbPrizeAmount.Size = new System.Drawing.Size(176, 37);
+            this.lbPrizeAmount.TabIndex = 25;
+            this.lbPrizeAmount.Text = "Prize Amount";
+            // 
+            // txtPrizePercentage
+            // 
+            this.txtPrizePercentage.Location = new System.Drawing.Point(236, 280);
+            this.txtPrizePercentage.Name = "txtPrizePercentage";
+            this.txtPrizePercentage.Size = new System.Drawing.Size(178, 35);
+            this.txtPrizePercentage.TabIndex = 28;
+            // 
+            // lbPrizePercentage
+            // 
+            this.lbPrizePercentage.AutoSize = true;
+            this.lbPrizePercentage.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPrizePercentage.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbPrizePercentage.Location = new System.Drawing.Point(18, 280);
+            this.lbPrizePercentage.Name = "lbPrizePercentage";
+            this.lbPrizePercentage.Size = new System.Drawing.Size(212, 37);
+            this.lbPrizePercentage.TabIndex = 27;
+            this.lbPrizePercentage.Text = "Prize Percentage";
+            // 
+            // lbOr
+            // 
+            this.lbOr.AutoSize = true;
+            this.lbOr.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbOr.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lbOr.Location = new System.Drawing.Point(194, 230);
+            this.lbOr.Name = "lbOr";
+            this.lbOr.Size = new System.Drawing.Size(64, 37);
+            this.lbOr.TabIndex = 29;
+            this.lbOr.Text = "-or-";
+            // 
+            // btnCreatePrize
+            // 
+            this.btnCreatePrize.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnCreatePrize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.btnCreatePrize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.btnCreatePrize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreatePrize.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCreatePrize.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCreatePrize.Location = new System.Drawing.Point(133, 342);
+            this.btnCreatePrize.Name = "btnCreatePrize";
+            this.btnCreatePrize.Size = new System.Drawing.Size(218, 54);
+            this.btnCreatePrize.TabIndex = 30;
+            this.btnCreatePrize.Text = "Create Prize";
+            this.btnCreatePrize.UseVisualStyleBackColor = true;
+            // 
+            // CreatePrizeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(449, 408);
+            this.Controls.Add(this.btnCreatePrize);
+            this.Controls.Add(this.lbOr);
+            this.Controls.Add(this.txtPrizePercentage);
+            this.Controls.Add(this.lbPrizePercentage);
+            this.Controls.Add(this.txtPlaceAmount);
+            this.Controls.Add(this.lbPrizeAmount);
+            this.Controls.Add(this.txtPlaceName);
+            this.Controls.Add(this.lbPlaceName);
+            this.Controls.Add(this.txtPlaceNumber);
+            this.Controls.Add(this.lbPlaceNumber);
+            this.Controls.Add(this.lbHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "CreatePrizeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create Prize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbHeader;
+        private TextBox txtPlaceNumber;
+        private Label lbPlaceNumber;
+        private TextBox txtPlaceName;
+        private Label lbPlaceName;
+        private TextBox txtPlaceAmount;
+        private Label lbPrizeAmount;
+        private TextBox txtPrizePercentage;
+        private Label lbPrizePercentage;
+        private Label lbOr;
+        private Button btnCreatePrize;
+    }
+}
