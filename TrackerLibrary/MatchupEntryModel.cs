65143d466a890a12ce93802e7287b3daff18f232
thanhnhan1402 <nhanttse151229@fpt.edu.vn>
2022-07-16 10:56:40 +0700

    Adding model classes

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represent one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represent the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represent the matchup that tis team came 
+        /// from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }       
+    }
+}
